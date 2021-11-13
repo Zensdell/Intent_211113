c0bba467329b09082ca4ad25c82c7a877bf4fa0c
구영모 <dudah754@naver.com>
2021-11-13 16:36:11 +0900

    주석 수정

--- app/src/main/java/com/nepplus/intent_211113/EditNicknameActivity.kt
+++ app/src/main/java/com/nepplus/intent_211113/EditNicknameActivity.kt
@@ -16,12 +16,12 @@ class EditNicknameActivity : AppCompatActivity() {
 //            1. 입력한 값을 변수에 저장
             val inputNickname = edtNickname.text.toString()
 
-//            2. 새 닉네임을 들고 복귀
+//            2. 새 닉네임을 들고 복귀 얘는 put으로 nick이름표이 edittext를 저장
 
             val resulutIntent = Intent()
             resulutIntent.putExtra("nick", inputNickname)
 
-//            3. 결과 설정 - OK 누른게 맞다고 구별, 들고갈 데이터
+//            3. 결과 설정 - OK 누른게 맞다고 구별, 들고갈 데이터  resulutIntent 통째로 들고 돌아가라
 
             setResult(RESULT_OK,resulutIntent)
 
