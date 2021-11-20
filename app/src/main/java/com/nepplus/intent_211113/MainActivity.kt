caf3d0f4633c8224d4cc9d8df9c95dae6ebea29c
구영모 <dudah754@naver.com>
2021-11-20 11:28:16 +0900

    카톡 플레이스토어 링크

--- app/src/main/java/com/nepplus/intent_211113/MainActivity.kt
+++ app/src/main/java/com/nepplus/intent_211113/MainActivity.kt
@@ -16,6 +16,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btnKakaoStoreLink.setOnClickListener {
+            val myUri = Uri.parse("market://details?id=com.kakao.talk")
+            val myIntent = Intent(Intent.ACTION_VIEW,myUri)
+            startActivity(myIntent)
+
+        }
+
 
         btnNaverWebLink.setOnClickListener {
 
